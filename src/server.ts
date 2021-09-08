1473f7a3788a2c57838fdc4c3818f915974943e1
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-09-07 23:57:45 -0300

    Created migrations and entity User

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import express from 'express'
+import 'reflect-metadata'
 
+import "./database";
 const app = express()
-
-app.get('/test', (request, response) => {
-  return response.send("Hi NLW Together")
-})
-
-app.post("/testPost", (request, response) => {
-  return response.send("Hi NLW - POST method")
-})
 
 app.listen(3001,
   () => console.log("Server is running - VALORIZA API")
