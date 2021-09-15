784aabd10cdfb48be4fce7b8912ab413eadddfbf
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-09-15 04:00:21 -0300

    Add Lesson#03 - Create Tag: Migration, Entite, Repositore, Service and Controller

---
+++ src/database/migrations/1631683456804-CreateTags.ts
@@ -0,0 +1,42 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table
+} from "typeorm";
+
+export class CreateTags1631683456804 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "tags",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar"
+          },
+          {
+            name: "created_at",
+            type: "timastamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("tags");
+  }
+
+}
